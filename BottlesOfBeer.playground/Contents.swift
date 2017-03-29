bf2b56941f539cf44fe2cec1bead93a416268328
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-03-28 22:41:14 -0500

    Update variable naming for clarity.

--- BottlesOfBeer.playground/Contents.swift
+++ BottlesOfBeer.playground/Contents.swift
@@ -1,10 +1,12 @@
 let maxBeers = 99
 let suffix = "of beer on the wall"
+
 func plural(_ n: Int) -> String { return n > 1 ? "s" : "" }
-for n in (0...maxBeers).reversed() {
-    if n > 0 {
-        let next = n - 1
-        print("\(n) bottle\(plural(n)) \(suffix), \(n) bottle\(plural(n)) of beer.")
+
+for count in (0...maxBeers).reversed() {
+    if count > 0 {
+        let next = count - 1
+        print("\(count) bottle\(plural(count)) \(suffix), \(count) bottle\(plural(count)) of beer.")
         print("Take one down and pass it around, ", terminator: "")
         if next > 0 {
             print("\(next) bottle\(plural(next)) \(suffix).")
